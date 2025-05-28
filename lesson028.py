9424e617a6d4078035d7b3f01c195900ecc978ff
Mark Peters <mapeters101@earthlink.net>
2025-05-28 17:18:54 -0400

    Work on lesson028.

---
+++ lesson028.py
@@ -0,0 +1,32 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print()
+for i in range(1, 4):
+    print(i)
+
+# print()
+# for i in range(1, a/b):
+#     print(i)
+
+print()
+
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)
+print(((a + b) / 3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+print(a / (b * a) / b)
