4f3cad14c396cdece209c6f908a94a5a59dc71a5
Mark Peters <mapeters101@earthlink.net>
2025-05-31 22:43:44 -0400

    Complete work on lesson030 from 1:20.

---
+++ lesson030.py
@@ -0,0 +1,31 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+
+print(parrot[3 - len(parrot)])
+print(parrot[4 - len(parrot)])
+print(parrot[9 - len(parrot)])
+print(parrot[3 - len(parrot)])
+print(parrot[6 - len(parrot)])
+print(parrot[8 - len(parrot)])
+
+
+
