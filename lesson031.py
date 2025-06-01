4c32fec34527794ef6d0e3f7abe7f36abf65995b
Mark Peters <mapeters101@earthlink.net>
2025-05-31 23:01:54 -0400

    Work on lesson031.

---
+++ lesson031.py
@@ -0,0 +1,29 @@
+#
+#         01234567890123
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])  # Norweg
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+print(parrot[:9])
+
+print(parrot[10:14])
+print(parrot[10:])
+print(parrot[-4:])
+
+print()
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(letters[::2])
+
+
+
+
+
