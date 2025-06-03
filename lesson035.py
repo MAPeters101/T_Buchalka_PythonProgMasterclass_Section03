452c220f9ba2a8093afe5ec9715400a98a45e358
Mark Peters <mapeters101@earthlink.net>
2025-06-03 18:50:15 -0400

    Work on lesson035.

---
+++ lesson035.py
@@ -0,0 +1,27 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[16:13:-1]
+print(backwards)
+
+backwards = letters[4::-1]
+print(backwards)
+
+backwards = letters[:17:-1]
+print(backwards)
+
+backwards = letters[:-9:-1]
+print(backwards)
+
+print('*'*80)
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[1:])
+print(letters[:1])
+print(letters[0])
+
+letters = ""
+print(letters[:1])
+#print(letters[0])
+
+
